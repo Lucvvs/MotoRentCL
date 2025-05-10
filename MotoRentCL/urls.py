b1915dad84496ab5447e01129ca3cb321a19cc4b
Lucvvs <lucas.olivos33@gmail.com>
2025-05-10 18:46:37 -0400

    rendr yaml

--- MotoRentCL/urls.py
+++ MotoRentCL/urls.py
@@ -4,7 +4,6 @@ from . import api_views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('alquiler.urls')),
     path('motos/',    api_views.lista_motos,    name='api-lista-motos'),
     path('reservas/', api_views.lista_reservas, name='api-lista-reservas'),
